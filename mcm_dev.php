c901774521aa76d262123cb3bd346a1ce763c0f6
Philipp Hager <office@phager.at>
2018-04-25 17:22:50 +0200

    add prototype of mcm_dev.php

---
+++ mcm_dev.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once(__DIR__ . '/../../config.php');
+
+require_login();
+if(!is_siteadmin()) {
+    header('Location: ' . $CFG->wwwroot . '/index.php');
+    exit;
+}
+
+define('HELPTEXT', "Possible actions (call as <cite>mcm_dev.php?action=CHOSENACTION</cite>):<br />
+<ul>
+    <li><b>clusteroff</b> deactivates all other nodes</li>
+    <li><b>status</b> outputs status information about all nodes</li>
+    <li><b>sync</b> copies data from current node to all others</li>
+    <li><b>clusteron</b> activates all nodes again</li>
+</ul>");
+
+header( 'Content-type: text/html; charset=utf-8' );
+
+// AJAX call handled here
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'clusteroff':
+            //$return = shell_exec('/opt/moodle/mcm-dev.sh -o0 other');
+            break;
+        case 'status':
+            $return = shell_exec('/opt/moodle/mcm-dev.sh -l');
+            break;
+        case 'sync':
+            //$return = shell_exec('/opt/moodle/mcm-dev.sh -s other');
+            break;
+        case 'clusteron':
+            //$return = shell_exec('/opt/moodle/mcm-dev.sh -o1 all');
+            break;
+        default:
+            $return = HELPTEXT;
+    }
+    echo converttoHTML($return);
+    die;
+} else {
+    $return = shell_exec('/opt/moodle/mcm-dev.sh -l');
+    echo "<section class='status'><h1>Current status:</h1><div>".convertoHTML($return)."</div></section>";
+    echo "<section class='status'><h1>HELP</h1><div>".converttoHTML(HELPTEXT)."</div></section>";
+}
+
+
+function convertToHTML($input) {
+    return str_replace(array(" ","\n", "\r", "\t"), array( '&nbsp;', '<br />', '<br />', '&nbsp;&nbsp;&nbsp;&nbsp;'), $input);//. '<br />';
+}
